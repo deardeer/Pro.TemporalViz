de4e7bca7d03ce019281432aa48383e8a2fc4478
soso <soso@someones-MacBook-Pro.local>
2020-04-17 22:34:51 +0800

    plot updated

--- server/handler/dbhandler.py
+++ server/handler/dbhandler.py
@@ -24,7 +24,7 @@ class getPlotHandler(tornado.web.RequestHandler):
 		self.set_header('Access-Control-Allow-Origin', '*');
 
 		dataset = "synthetic_3"  # options: countries, coronavirus_china, coronavirus_us, synthetic_1, synthetic_2, synthetic_3
-		dimensional_reduction = "t_pca"  # options: t_pca, pca
+		dimensional_reduction = "pca"  # options: t_pca, pca
 
 		print(f"dataset={dataset}, method={dimensional_reduction}")
 		print(f"Loading Data...")
